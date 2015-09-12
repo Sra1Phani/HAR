29b2c965ed4d44cd748b60f9fd0a96695661d8ac
Sravan Phani Kumar Vidiyala <sravan.phani@gmail.com>
2015-09-12 21:32:03 +0530

    complete har code upload

--- HAR.R
+++ HAR.R
@@ -12,6 +12,7 @@ names(subjects) <- 's'
 View(subjects)
 #Training set
 metrics <- read.delim("/Users/sra1/Documents/UCI HAR Dataset/train/X_train.txt",header=F,sep="")
+View(metrics)
 
 #Training set labels
 act_labels <- read.csv("/Users/sra1/Documents/UCI HAR Dataset/train/y_train.txt",header=F)
@@ -69,28 +70,28 @@ View(grpsd_act)
 # Function for calculating within group variance
 calcWithinGroupsVariance <- function(variable,groupvariable)
 {
-  # find out how many values the group variable can take
-  groupvariable2 <- as.factor(groupvariable[[1]])
-  levels <- levels(groupvariable2)
-  numlevels <- length(levels)
-  # get the mean and standard deviation for each group:
-  numtotal <- 0
-  denomtotal <- 0
-  for (i in 1:numlevels)
-  {
-    leveli <- levels[i]
-    levelidata <- variable[groupvariable==leveli,]
-    levelilength <- length(levelidata)
-    # get the standard deviation for group i:
-    sdi <- sd(levelidata)
-    numi <- (levelilength - 1)*(sdi * sdi)
-    denomi <- levelilength
-    numtotal <- numtotal + numi
-    denomtotal <- denomtotal + denomi
-  }
-  # calculate the within-groups variance
-  Vw <- numtotal / (denomtotal - numlevels)
-  return(Vw)
+    # find out how many values the group variable can take
+    groupvariable2 <- as.factor(groupvariable[[1]])
+    levels <- levels(groupvariable2)
+    numlevels <- length(levels)
+    # get the mean and standard deviation for each group:
+    numtotal <- 0
+    denomtotal <- 0
+    for (i in 1:numlevels)
+    {
+        leveli <- levels[i]
+        levelidata <- variable[groupvariable==leveli,]
+        levelilength <- length(levelidata)
+        # get the standard deviation for group i:
+        sdi <- sd(levelidata)
+        numi <- (levelilength - 1)*(sdi * sdi)
+        denomi <- levelilength
+        numtotal <- numtotal + numi
+        denomtotal <- denomtotal + denomi
+    }
+    # calculate the within-groups variance
+    Vw <- numtotal / (denomtotal - numlevels)
+    return(Vw)
 }
 
 # within groups variance by subjects
@@ -99,7 +100,7 @@ grpvarbysub <- list()
 
 for (i in 3:ln)
 {
-  grpvarbysub <- c(grpvarbysub,calcWithinGroupsVariance(dataset[i],dataset[1]))
+    grpvarbysub <- c(grpvarbysub,calcWithinGroupsVariance(dataset[i],dataset[1]))
 }
 
 grpvarbysub <- data.frame(grpvarbysub)
@@ -113,7 +114,7 @@ grpvarbyact <- list()
 
 for (i in 3:ln)
 {
-  grpvarbyact <- c(grpvarbyact,calcWithinGroupsVariance(dataset[i],dataset[2]))
+    grpvarbyact <- c(grpvarbyact,calcWithinGroupsVariance(dataset[i],dataset[2]))
 }
 
 grpvarbyact <- data.frame(grpvarbyact)
@@ -125,32 +126,32 @@ View(grpvarbyact)
 # Function for calculation of between group variance
 calcBetweenGroupsVariance <- function(variable,groupvariable)
 {
-  # find out how many values the group variable can take
-  groupvariable2 <- as.factor(groupvariable[[1]])
-  levels <- levels(groupvariable2)
-  numlevels <- length(levels)
-  # calculate the overall grand mean:
-  grandmean <- mean(variable)
-  # get the mean and standard deviation for each group:
-  numtotal <- 0
-  denomtotal <- 0
-  for (i in 1:numlevels)
-  {
-    leveli <- levels[i]
-    levelidata <- variable[groupvariable==leveli]
-    levelilength <- length(levelidata)
-    # get the mean and standard deviation for group i:
-    meani <- mean(levelidata)
-    sdi <- sd(levelidata)
-    numi <- levelilength * ((meani - grandmean)^2)
-    denomi <- levelilength
-    numtotal <- numtotal + numi
-    denomtotal <- denomtotal + denomi
-  }
-  # calculate the between-groups variance
-  Vb <- numtotal / (numlevels - 1)
-  Vb <- Vb[[1]]
-  return(Vb)
+    # find out how many values the group variable can take
+    groupvariable2 <- as.factor(groupvariable[[1]])
+    levels <- levels(groupvariable2)
+    numlevels <- length(levels)
+    # calculate the overall grand mean:
+    grandmean <- mean(variable)
+    # get the mean and standard deviation for each group:
+    numtotal <- 0
+    denomtotal <- 0
+    for (i in 1:numlevels)
+    {
+        leveli <- levels[i]
+        levelidata <- variable[groupvariable==leveli]
+        levelilength <- length(levelidata)
+        # get the mean and standard deviation for group i:
+        meani <- mean(levelidata)
+        sdi <- sd(levelidata)
+        numi <- levelilength * ((meani - grandmean)^2)
+        denomi <- levelilength
+        numtotal <- numtotal + numi
+        denomtotal <- denomtotal + denomi
+    }
+    # calculate the between-groups variance
+    Vb <- numtotal / (numlevels - 1)
+    Vb <- Vb[[1]]
+    return(Vb)
 }
 
 # between groups variance by subjects
@@ -160,7 +161,7 @@ calcBetweenGroupsVariance(dataset[,3],dataset[1])
 
 for (i in 3:ln)
 {
-  btwgrpvarbysub <- c(btwgrpvarbysub,calcBetweenGroupsVariance(dataset[,i],dataset[1]))
+    btwgrpvarbysub <- c(btwgrpvarbysub,calcBetweenGroupsVariance(dataset[,i],dataset[1]))
 }
 
 btwgrpvarbysub <- data.frame(btwgrpvarbysub)
@@ -176,7 +177,7 @@ calcBetweenGroupsVariance(dataset[,3],dataset[2])
 
 for (i in 3:ln)
 {
-  btwgrpvarbyact <- c(btwgrpvarbyact,calcBetweenGroupsVariance(dataset[,i],dataset[2]))
+    btwgrpvarbyact <- c(btwgrpvarbyact,calcBetweenGroupsVariance(dataset[,i],dataset[2]))
 }
 
 btwgrpvarbyact <- data.frame(btwgrpvarbyact)
@@ -204,29 +205,29 @@ View(sepbyact)
 #class_file <- as.factor(file3$V1)
 #dataset <- cbind(file1,file2,class_file)
 
-## Correlation matrix with p-values. 
+## Correlation matrix with p-values.
 ## See http://goo.gl/nahmV for documentation of this function
 cor.prob <- function (X, dfr = nrow(X) - 2) {
-  R <- cor(X, use="pairwise.complete.obs")
-  above <- row(R) < col(R)
-  r2 <- R[above]^2
-  Fstat <- r2 * dfr/(1 - r2)
-  R[above] <- 1 - pf(Fstat, 1, dfr)
-  R[row(R) == col(R)] <- NA
-  R
+    R <- cor(X, use="pairwise.complete.obs")
+    above <- row(R) < col(R)
+    r2 <- R[above]^2
+    Fstat <- r2 * dfr/(1 - r2)
+    R[above] <- 1 - pf(Fstat, 1, dfr)
+    R[row(R) == col(R)] <- NA
+    R
 }
 
 ## Use this to dump the cor.prob output to a 4 column matrix
 ## with row/column indices, correlation, and p-value.
 ## See StackOverflow question: http://goo.gl/fCUcQ
 flattenSquareMatrix <- function(m) {
-  if( (class(m) != "matrix") | (nrow(m) != ncol(m))) stop("Must be a square matrix.") 
-  if(!identical(rownames(m), colnames(m))) stop("Row and column names must be equal.")
-  ut <- upper.tri(m)
-  data.frame(i = rownames(m)[row(m)[ut]],
-             j = rownames(m)[col(m)[ut]],
-             cor=t(m)[ut],
-             p=m[ut])
+    if( (class(m) != "matrix") | (nrow(m) != ncol(m))) stop("Must be a square matrix.")
+    if(!identical(rownames(m), colnames(m))) stop("Row and column names must be equal.")
+    ut <- upper.tri(m)
+    data.frame(i = rownames(m)[row(m)[ut]],
+    j = rownames(m)[col(m)[ut]],
+    cor=t(m)[ut],
+    p=m[ut])
 }
 
 
@@ -235,16 +236,21 @@ flattenSquareMatrix <- function(m) {
 datpval <- flattenSquareMatrix(cor.prob(dataset[3:563]))
 datpval <- datpval[order(-datpval[,3],datpval[,4]),]
 View(datpval)
+View(dataset[1:2])
+
 
 #Standardising the metrics dataset
 stdmetrics <- as.data.frame(scale(dataset[3:563]))
 View(stdmetrics)
+
 #Checking for standardised metrics dataset
 sapply(stdmetrics,mean)
 sapply(stdmetrics,sd)
 
 #applying Principal Component Analysis
 pcametrics <- princomp(stdmetrics)
+loadings(pcametrics)
+
 
 #Summary of Principal Component Analysis
 summary(pcametrics)
@@ -253,8 +259,9 @@ screeplot(pcametrics,type='lines')
 plot(pcametrics)
 
 plot(pcametrics$loadings[,1],pcametrics$loadings[,2])
-text(pcametrics$x[,1],pcametrics$x[,2], dataset$s, cex=0.7, pos=4, col="red")
+#text(pcametrics$x[,1],pcametrics$x[,2], dataset$s, cex=0.7, pos=4, col="red")
 text(pcametrics$loadings[,1],pcametrics$loadings[,2], dataset$a, cex=0.7, pos=4, col="red")
+
 
 var1<-predict(pcametrics)[,1]
 var2<-predict(pcametrics)[,2]
@@ -317,23 +324,43 @@ var58<-predict(pcametrics)[,58]
 var59<-predict(pcametrics)[,59]
 var60<-predict(pcametrics)[,60]
 var61<-predict(pcametrics)[,61]
+View(metrics)
+View(stdmetrics)
 
-#model
-model=lm(dataset$a~var1+var2+var3+var4+var5+var6+var7+
-           var8+var9+var10+var11+var12+var13+var14+
-           var15+var16+var17+var18+var19+var21+
-           var22+var23+var24+var25+var26+var27+var28+
-           var30+var31+var32+var33+var34+var35+
-           var36+var37+var38+var39+var40+var42+
-           var43+var44+var45+var46+var47+var48+
-           var50+var51+var52+var53+var55+var56+
-           var57+var59+var61)
+#-----------------------------------------------------------------------------------------------
 
-summary(model)
-20,29,41,49,54,58,60
+predset <- cbind(dataset$a,var1,var2,var3,var4,var5,var6,var7,
+var8,var9,var10,var11,var12,var13,var14,
+var15,var16,var17,var18,var19,var21,
+var22,var23,var24,var25,var26,var27,var28,
+var30,var31,var32,var33,var34,var35,
+var36,var37,var38,var39,var40,var42,
+var43,var44,var45,var46,var47,var48,
+var50,var51,var52,var53,var55,var56,
+var57,var59,var61)
 
-ds <- dataset[,-1]
-View(ds)
-attach(ds)
-model2 <- lm(a~.)
-save.image("~/Documents/Insofe/HAR/HAR.RData")
+View(predset)
+class(predset)
+
+predset <- data.frame(predset)
+
+predset$V1 <- as.factor(predset$V1)
+
+
+#########Oblique trees
+View(predset)
+library(oblique.tree)
+detach(predset)
+
+dtOb = oblique.tree(V1~., data = predset, oblique.splits = "only")
+summary(dtOb)
+
+plot(dtOb)
+
+a <- table(predset$V1, predict(dtOb, newdata=predset, type="class"))
+
+sum(diag(a))/sum(a)*100
+
+a=table(test$quality, predict(dtOb, newdata=test, type="class"))
+
+a[2,2]/(a[1,2]+a[2,2])*100
